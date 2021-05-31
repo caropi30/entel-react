e37c673620ff3898813fb16a0f8e09273c5a4de2
Carolina Pirela <caropi30@gmail.com>
2021-05-31 16:17:10 -0400

    agregando icono a CartWidget

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
-import { Cart } from 'react-bootstrap-icons';
+import * as Icon  from 'react-bootstrap-icons';
 
 const CartWidget = () => {
     return (
         <Button variant="primary" size="lg">
-            <Cart />
+            <Icon.Cart />
         </Button>
     )
 };
